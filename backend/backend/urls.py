ed4d287524087308aa701edeb170e3d8f68a8c89
Mario Dariel <mdariel663@gmail.com>
2024-08-23 02:18:12 -0400

    Update Archs

--- backend/backend/urls.py
+++
@@ -1,9 +0,0 @@
-# backend/urls.py
-from django.contrib import admin
-from django.urls import path, include
-from routing import Router
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include(Router)),
-]
